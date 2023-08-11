90569168d2f5f6222bac15ff7db91ca1c8b823ed
LudaKend <Ludakend@gmail.com>
2023-08-11 18:24:28 +0300

    дополнительные проверки для 100% покрытия

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -7,8 +7,21 @@ def test_get():
     #исправляем:
     assert arrs.get([1, 2, 3], 1, "test") == 2
     assert arrs.get([], 0, "test") == "test"
-
+#дополнительные проверки для 100% покрытия
+    assert arrs.get([1, 2, 3], -1, "test") == "test"
+    assert arrs.get(['1', '2', '3'], 1, "test") == '2'
+    assert arrs.get([], 0) == None
+#    assert arrs.get([1, 2, 3], 5, "test") == "test"
+#    assert arrs.get([1, 2, 3]) == "test"
 
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+#дополнительные проверки для 100% покрытия
+    assert arrs.my_slice([1, 2, 3], -1) == [3]
+    assert arrs.my_slice([1, 2, 3, 4], -1, -1) == []
+    assert arrs.my_slice([], 1) == []
+    assert arrs.my_slice([]) == []
+    assert arrs.my_slice(['1', '2', '3'], 1) == ['2', '3']
+    assert arrs.my_slice([1, 2, 3, 4], 0, -1) == [1, 2, 3]
+    assert arrs.my_slice([1, 2, 3, 4], -5) == [1, 2, 3, 4]
